23321b02cf7bee8b34b435e2c05adcecf66c2b58
uael <lucas.abel@yandex.com>
2018-03-14 13:11:00 +0100

    pause it

--- src/nc/update.c
+++ src/nc/update.c
@@ -12,18 +12,34 @@
 
 #include "internal.h"
 
-int	cw_nc_update(t_cw *cw)
+static void	nc_pause(int *running)
+{
+	*running = 0;
+	while (getch() != 32)
+		;
+	*running = 1;
+}
+
+int			cw_nc_update(t_cw *cw)
 {
 	int ch;
+	static int running = 0;
 
 	if (!cw->opt.n)
 		return (YEP);
+	if (!running)
+		nc_pause(&running);
 	if ((ch = getch()) != ERR)
 	{
 		if (ch == 27)
 			return (cw_exit(EXIT_SUCCESS, "Good bye!\n"));
-		ft_printf("got %d\n", ch);
-		cw_nc_notify(cw, (uint16_t)ch, (uint16_t)(ch % 8), (uint8_t)ch);
+		else if (ch == 32)
+			nc_pause(&running);
+		else
+		{
+			ft_printf("got %d\n", ch);
+			cw_nc_notify(cw, (uint16_t)ch, (uint16_t)(ch % 8), (uint8_t)ch);
+		}
 	}
 	return (YEP);
 }
