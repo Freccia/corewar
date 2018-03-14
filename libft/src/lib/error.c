ef6bff6e57af083fc517c7fe530f7641a11b25f5
JeanMax <mcanal@student.42.fr>
2018-03-14 01:27:32 +0100

    Fix libft (pingu)

--- libft/src/lib/error.c
+++ libft/src/lib/error.c
@@ -6,11 +6,14 @@
 /*   By: alucas- <alucas-@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 09:44:14 by alucas-           #+#    #+#             */
-/*   Updated: 2017/11/08 14:29:11 by alucas-          ###   ########.fr       */
+/*   Updated: 2018/03/14 01:25:17 by mc               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <errno.h>
+#ifndef EQFULL
+# define EQFULL 106
+#endif
 
 int	ft_error(int rno, int eno)
 {
