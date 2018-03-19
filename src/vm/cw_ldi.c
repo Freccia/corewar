b235569bfe883fff7ff7ccb489cd4a2fe674a510
fxrc <Freccia@users.noreply.github.com>
2018-03-19 17:57:12 +0100

    cw_sti++

--- src/vm/cw_ldi.c
+++ src/vm/cw_ldi.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 19:10:18 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/03/19 16:48:14 by lfabbro          ###   ########.fr       */
+/*   Updated: 2018/03/19 17:47:37 by lfabbro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int			cw_ldi(t_proc *proc, uint8_t *pc)
 	reg = *ptr;
 	if (!reg || reg > REG_NUMBER)
 		return (EXIT_FAILURE);
-	read = cw_move_ptr(pc, (a[0] + a[1]) % IDX_MOD); // %IDX_MOD ??
+	read = cw_move_ptr(pc, (a[0] + a[1]) % IDX_MOD); // TODO: %IDX_MOD ??
 	proc->reg[reg] = ft_mtoi(cw_map_mem(mem, read), 1);
 	cw_update_carry(proc, g_cw->current->reg[reg]);
 	proc->pc = cw_move_ptr(pc, 8);
--- src/vm/cw_sti.c
+++ src/vm/cw_sti.c
@@ -6,15 +6,30 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 19:16:47 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/03/18 15:09:51 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/03/19 17:56:30 by lfabbro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-int			cw_sti(t_proc *proc, uint8_t *mem)
+int			cw_sti(t_proc *proc, uint8_t *pc)
 {
-	(void)proc;
-	(void)mem;
-	return (YEP);
+	uint8_t		*ptr;
+	uint32_t	*write;
+	uint32_t	a[2];
+	uint8_t		reg;
+
+	ptr = cw_move_ptr(pc, 2);
+	reg = *ptr;
+	ptr = cw_move_ptr(pc, 1);
+	a[0] = cw_read_arg(proc, &ptr, 0, E_IND_SHORT);
+	a[1] = cw_read_arg(proc, &ptr, 1, E_IND_SHORT);
+	if (!reg || reg > REG_NUMBER)
+		return (EXIT_FAILURE);
+	// TODO: %IDX_MOD ??
+	write = (uint32_t*)cw_move_ptr(pc, (a[0] + a[1]) % IDX_MOD);
+	*write = proc->reg[reg];
+	cw_update_carry(proc, *write);
+	proc->pc = cw_move_ptr(pc, 8);
+	return (EXIT_SUCCESS);
 }
