5af097a26ca86333a4136d7c7f152c1fc5360d5c
fxrc <Freccia@users.noreply.github.com>
2018-03-19 15:02:52 +0100

    cw_add++ cw_sub++ cw_ldi=fix

--- src/vm/cw_add.c
+++ src/vm/cw_add.c
@@ -6,15 +6,28 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 19:08:36 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/03/18 15:07:46 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/03/19 15:00:50 by lfabbro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-int			cw_add(t_proc *proc, uint8_t *mem)
+int			cw_add(t_proc *proc, uint8_t *pc)
 {
-	(void)proc;
-	(void)mem;
-	return (YEP);
+	uint8_t		*ptr;
+	uint8_t		a[3];
+
+	ptr = cw_move_ptr(pc, 2);
+	a[0] = *ptr;
+	ptr = cw_move_ptr(pc, 1);
+	a[1] = *ptr;
+	ptr = cw_move_ptr(pc, 1);
+	a[2] = *ptr;
+	if (a[0] == 0 || a[0] > REG_NUMBER || a[1] == 0 || a[1] > REG_NUMBER ||
+		a[2] == 0 || a[2] > REG_NUMBER)
+		return (EXIT_FAILURE);
+	proc->reg[a[2]] = proc->reg[a[0]] + proc->reg[a[1]];
+	cw_update_carry(proc, proc->reg[a[2]]);
+	proc->pc = cw_move_ptr(pc, 5);
+	return (EXIT_SUCCESS);
 }
--- src/vm/cw_ldi.c
+++ src/vm/cw_ldi.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 19:10:18 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/03/19 14:47:44 by lfabbro          ###   ########.fr       */
+/*   Updated: 2018/03/19 14:59:00 by lfabbro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,9 @@ int			cw_ldi(t_proc *proc, uint8_t *pc)
 	reg = *ptr;
 	if (!reg || reg > REG_NUMBER)
 		return (EXIT_FAILURE);
-	read = cw_move_ptr(pc, a[0] + a[1]);
-	g_cw->current->reg[reg] = ft_mtoi(cw_map_mem(mem, read), 1);
+	read = cw_move_ptr(pc, (a[0] + a[1]) % IDX_MOD);
+	proc->reg[reg] = ft_mtoi(cw_map_mem(mem, read), 1);
 	cw_update_carry(proc, g_cw->current->reg[reg]);
-	proc->pc = cw_move_ptr(ptr, ptr - pc);
+	proc->pc = cw_move_ptr(pc, 8);
 	return (EXIT_SUCCESS);
 }
