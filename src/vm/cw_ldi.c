66bef450c4be03ac3d834d48cb20266ce0025f19
fxrc <Freccia@users.noreply.github.com>
2018-03-19 14:47:57 +0100

    norme

--- src/vm/cw_ldi.c
+++ src/vm/cw_ldi.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 19:10:18 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/03/19 14:40:09 by lfabbro          ###   ########.fr       */
+/*   Updated: 2018/03/19 14:47:44 by lfabbro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,17 @@ int			cw_ldi(t_proc *proc, uint8_t *pc)
 	uint8_t		*ptr;
 	uint8_t		*read;
 	uint8_t		mem[4];
-	uint32_t	a1;
-	uint32_t	a2;
+	uint32_t	a[2];
 	uint8_t		reg;
 
 	ptr = cw_move_ptr(pc, 2);
-	a1 = cw_read_arg(pc, &ptr, 0, 2);
-	a2 = cw_read_arg(pc, &ptr, 1, 2);
+	a[0] = cw_read_arg(pc, &ptr, 0, 2);
+	a[1] = cw_read_arg(pc, &ptr, 1, 2);
 	ptr = cw_move_ptr(ptr, 1);
 	reg = *ptr;
 	if (!reg || reg > REG_NUMBER)
 		return (EXIT_FAILURE);
-	read = cw_move_ptr(pc, a1 + a2);
+	read = cw_move_ptr(pc, a[0] + a[1]);
 	g_cw->current->reg[reg] = ft_mtoi(cw_map_mem(mem, read), 1);
 	cw_update_carry(proc, g_cw->current->reg[reg]);
 	proc->pc = cw_move_ptr(ptr, ptr - pc);
