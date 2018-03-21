f4e012fdf882efec04764fe16ec3db0de577a973
jon-finkel <nfinkel@student.42.fr>
2018-03-21 21:26:33 +0100

    Added cw_verbose.

--- src/vm/cw_live.c
+++ src/vm/cw_live.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 19:10:47 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/03/21 20:40:45 by lfabbro          ###   ########.fr       */
+/*   Updated: 2018/03/21 21:22:08 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,13 @@ int			cw_live(t_proc *proc, uint8_t *pc)
 		{
 			//proc->lastlive = g_cw->cycle;
 			champ->lastlive = g_cw->cycle;
-			ft_printf("Player %s [%hd] is alive!\n", champ->name, champ->id);
+			cw_verbose(proc, champ->name, champ->id, E_VALID_LIVE);
 			proc->pc = cw_move_ptr(pc, 4);
 			return (EXIT_SUCCESS);
 		}
 		champ = champ->next;
 	}
-	ft_printf("A live has been made... But nobody came.\n");
+	cw_verbose(proc, champ->name, champ->id, E_INVALID_LIVE);
 	proc->pc = cw_move_ptr(pc, 4);
 	return (EXIT_SUCCESS);
 }
