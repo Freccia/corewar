37456f7509a9b42221e63ab7938ca170c5cba1ef
freccia <Freccia@users.noreply.github.com>
2018-03-22 18:38:52 +0100

    fix fork

--- src/vm/cw_fork.c
+++ src/vm/cw_fork.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 19:09:54 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/03/22 01:40:31 by lfabbro          ###   ########.fr       */
+/*   Updated: 2018/03/22 18:38:25 by lfabbro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ int			cw_fork(t_proc *proc, uint8_t *pc)
 {
 	t_proc		*new;
 	uint8_t		*ptr;
-	int32_t		n;
+	int16_t		n;
 
 	ptr = cw_move_ptr(pc, 1);
-	n = cw_read_arg(proc, &ptr, 0, F_DIR);
+	n = cw_read_n(ptr, 2);
 	new = ft_malloc(sizeof(t_proc));
 	new->next = g_cw->procs;
 	g_cw->procs = new;
--- src/vm/vm_run.c
+++ src/vm/vm_run.c
@@ -6,7 +6,7 @@
 /*   By: lfabbro <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/16 16:55:56 by lfabbro           #+#    #+#             */
-/*   Updated: 2018/03/22 01:38:38 by lfabbro          ###   ########.fr       */
+/*   Updated: 2018/03/22 18:32:30 by lfabbro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -145,13 +145,13 @@ void	cw_vm_eval(t_proc *proc)
 		--proc->wait;
 	else if (cw_vm_exec(proc, proc->pc) == EXIT_FAILURE)
 	{
+		//ft_dprintf(1, "Proc %d has crashed: %d.\n", proc->id, *proc->pc);
 		// L'update du curseur sur le GUI est foireux, mais le pc se deplace bien
-		cw_nc_notify(proc->pc - g_cw->mem, 1, *proc->pc);
+		cw_nc_notify(proc->pc - g_cw->mem, proc->id, *proc->pc);
 		// TODO: proc->pc is updated directly on each instruction (see: zjmp)
 		//proc->pc = cw_move_ptr(proc->pc, 1);
 		// TODO: wait must be equal to the next instruction cycles
-		proc->wait = g_op_tab[*proc->pc].cycles;
-		cw_nc_notify(proc->pc - g_cw->mem, 1, *proc->pc);
+		proc->wait = g_op_tab[*proc->pc - 1].cycles;
 	}
 	else if (*proc->pc >= 0x1 && *proc->pc <= MAX_OP)
 		proc->wait = g_op_tab[*proc->pc - 1].cycles - 1;
@@ -160,7 +160,7 @@ void	cw_vm_eval(t_proc *proc)
 		proc->wait = 1;
 		// TODO ok ? (if the instruction is wrong the pc keeps mooving)
 		// or we crash it ?
-		proc->pc = cw_move_ptr(proc->pc, 1);
+		//proc->pc = cw_move_ptr(proc->pc, 1);
 	}
 }
 
