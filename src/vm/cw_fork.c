7707cc1a17c3d73128d29406f94901f6d6fcd049
freccia <Freccia@users.noreply.github.com>
2018-03-22 18:43:04 +0100

    fix fix fix fork!

--- src/vm/cw_fork.c
+++ src/vm/cw_fork.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 19:09:54 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/03/22 18:38:25 by lfabbro          ###   ########.fr       */
+/*   Updated: 2018/03/22 18:42:07 by lfabbro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,13 @@ int			cw_fork(t_proc *proc, uint8_t *pc)
 	ptr = cw_move_ptr(pc, 1);
 	n = cw_read_n(ptr, 2);
 	new = ft_malloc(sizeof(t_proc));
-	new->next = g_cw->procs;
-	g_cw->procs = new;
 	ft_memcpy(new, proc, sizeof(t_proc));
 	new->pc = cw_move_ptr(pc, n % IDX_MOD);
-	new->next = NULL;
 	proc->pc = cw_move_ptr(pc, 3);
 	if (*proc->pc >= 1 && *proc->pc <= MAX_OP)
 		new->wait = g_op_tab[*proc->pc - 1].cycles - 1;
+	new->next = g_cw->procs;
+	g_cw->procs = new;
 	++g_cw->proc_count;
 	return (EXIT_SUCCESS);
 }
