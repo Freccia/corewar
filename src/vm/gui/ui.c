244f25ab7c37dfc7fe5739114709518eb56523d1
freccia <Freccia@users.noreply.github.com>
2018-03-29 19:36:09 +0200

    update gui registers

--- src/vm/gui/ui.c
+++ src/vm/gui/ui.c
@@ -110,14 +110,14 @@ void		vm_guiproc(t_proc *proc)
 	else
 		++y;
 	++y;
-	mvwprintw(g_wprocs, ++y, 4, "  01 | %08x %08x | 02", proc->reg[reg1 += 2], proc->reg[reg2 += 2]);
-	mvwprintw(g_wprocs, ++y, 4, "  03 | %08x %08x | 04", proc->reg[reg1 += 2], proc->reg[reg2 += 2]);
-	mvwprintw(g_wprocs, ++y, 4, "  05 | %08x %08x | 06", proc->reg[reg1 += 2], proc->reg[reg2 += 2]);
-	mvwprintw(g_wprocs, ++y, 4, "  07 | %08x %08x | 08", proc->reg[reg1 += 2], proc->reg[reg2 += 2]);
-	mvwprintw(g_wprocs, ++y, 4, "  09 | %08x %08x | 0A", proc->reg[reg1 += 2], proc->reg[reg2 += 2]);
-	mvwprintw(g_wprocs, ++y, 4, "  0B | %08x %08x | 0C", proc->reg[reg1 += 2], proc->reg[reg2 += 2]);
-	mvwprintw(g_wprocs, ++y, 4, "  0D | %08x %08x | 0E", proc->reg[reg1 += 2], proc->reg[reg2 += 2]);
-	mvwprintw(g_wprocs, ++y, 4, "  0F | %08x %08x | 10", proc->reg[reg1], proc->reg[reg2]);
+	mvwprintw(g_wprocs, ++y, 4, "  01 | 0x%08x 0x%08x | 02", proc->reg[reg1 += 2], proc->reg[reg2 += 2]);
+	mvwprintw(g_wprocs, ++y, 4, "  03 | 0x%08x 0x%08x | 04", proc->reg[reg1 += 2], proc->reg[reg2 += 2]);
+	mvwprintw(g_wprocs, ++y, 4, "  05 | 0x%08x 0x%08x | 06", proc->reg[reg1 += 2], proc->reg[reg2 += 2]);
+	mvwprintw(g_wprocs, ++y, 4, "  07 | 0x%08x 0x%08x | 08", proc->reg[reg1 += 2], proc->reg[reg2 += 2]);
+	mvwprintw(g_wprocs, ++y, 4, "  09 | 0x%08x 0x%08x | 0A", proc->reg[reg1 += 2], proc->reg[reg2 += 2]);
+	mvwprintw(g_wprocs, ++y, 4, "  0B | 0x%08x 0x%08x | 0C", proc->reg[reg1 += 2], proc->reg[reg2 += 2]);
+	mvwprintw(g_wprocs, ++y, 4, "  0D | 0x%08x 0x%08x | 0E", proc->reg[reg1 += 2], proc->reg[reg2 += 2]);
+	mvwprintw(g_wprocs, ++y, 4, "  0F | 0x%08x 0x%08x | 10", proc->reg[reg1], proc->reg[reg2]);
 	wattr_off(g_wprocs, 0x200000, 0x0);
 	wrefresh(g_wprocs);
 }
