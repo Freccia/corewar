4062450cec370d98e2416a468d0a1c2eb5d37c6f
uael <lucas.abel@yandex.com>
2018-03-30 14:22:56 +0200

    zaz behavior

--- src/vm/instr/add.c
+++ src/vm/instr/add.c
@@ -18,10 +18,10 @@ int			vm_add(t_proc *proc, uint8_t *pc)
 	int32_t av[3];
 
 	ptr = vm_move(pc, 2, FALSE);
-	av[0] = vm_readarg(proc, &ptr, 0, F_REG_VAL);
-	av[1] = vm_readarg(proc, &ptr, 1, F_REG_VAL);
-	av[2] = vm_readarg(proc, &ptr, 2, F_REG);
-	proc->pc = vm_move(pc, 5, FALSE);
+	av[0] = vm_readarg(proc, &ptr, 0, F_REG_VAL | F_IND);
+	av[1] = vm_readarg(proc, &ptr, 1, F_REG_VAL | F_IND);
+	av[2] = vm_readarg(proc, &ptr, 2, F_REG | F_IND);
+	proc->pc = ptr;
 	if (av[2] < 0x1 || av[2] > REG_NUMBER)
 		return (EXIT_FAILURE);
 	proc->reg[av[2]] = av[0] + av[1];
